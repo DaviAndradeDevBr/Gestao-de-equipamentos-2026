4c2e4d8e3b74b27e815324ed79b02244ef43a6b0
Davi Andrade <contatodaviandrade20@gmail.com>
2026-04-23 15:38:40 -0300

    Adicionada visualização de Equipamentos

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -135,6 +135,8 @@ while (true)
             );
         }
 
+        Console.WriteLine("---------------------------------");
+
         string? idSelecionado;
 
         do
@@ -300,5 +302,33 @@ while (true)
 
     else if (opcaoMenu == "4")
     {
+        Console.Clear();
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Gestão de Equipamentos");
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Visualização de Equipamentos");
+        Console.WriteLine("---------------------------------");
+
+        Console.WriteLine(
+            "{0, -7} | {1, -15} | {2, -15} | {3, -22} | {4, -10}",
+            "Id", "Nome", "Fabricante", "Preço de Aquisição", "Data de Fabricação"
+        );
+
+        for (int i = 0; i < equipamentos.Length; i++)
+        {
+            Equipamento? e = equipamentos[i];
+
+            if (e == null)
+                continue;
+
+            Console.WriteLine(
+                "{0, -7} | {1, -15} | {2, -15} | {3, -22} | {4, -10}",
+                e.id, e.nome, e.fabricante, e.precoAquisicao.ToString("C2"), e.dataFabricacao.ToShortDateString()
+            );
+        }
+
+        Console.WriteLine("---------------------------------");
+        Console.Write("Pressione ENTER para continuar...");
+        Console.ReadLine();
     }
 }
